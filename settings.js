9e18a01a3f43df5efd70786b47236ce8b105bbce
Professor Sahil <sahil.malume@gmail.com>
2026-06-29 14:45:48 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'PrimeSA_Bot V1.0',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "PrimeSA_Bot V1.0",
+  botOwner: 'Professor Sahil', // Your name
+  ownerNumber: '27835515085', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.7",
+  version: "1.0.0",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
